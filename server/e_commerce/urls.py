252ab2decbcfeea412b596e9dcb8e4301494a804
johanriascos7777 <johanriascos7777@gmail.com>
2025-05-12 00:28:37 -0500

    feat: Implement core category app structure including models, views, URLs, admin integration, and initial migration, while also rectifying critical admin panel access issues by resolving a TemplateDoesNotExist error caused by a conflicting catch-all URL pattern and addressing login failures due to an incorrect 'is_active' status on the superuser account, thereby restoring full administrative functionality.

--- server/e_commerce/urls.py
+++ server/e_commerce/urls.py
@@ -8,8 +8,12 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.social.urls')),
+    
+    path('api/category/', include('apps.category.urls')),
+    
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += [re_path(r'^.*',
-                        TemplateView.as_view(template_name='index.html'))]
+#NO necesito esta línea si el frontend se sirve desde otro dominio/puerto/servidor 7777
+#urlpatterns += [re_path(r'^.*',
+#                        TemplateView.as_view(template_name='index.html'))]
